cb7eb638d0fb23339f35b76899c9eaba4c00d92b
albinj12 <albinavirappatt@gmail.com>
2021-09-05 12:00:28 +0530

    queue using linked list

---
+++ Queue/queue_linked_list.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+type node struct {
+	data int
+	next *node
+}
+
+type queue struct {
+	front *node
+	rear  *node
+}
+
+func main() {
+	q := queue{}
+}
+
+func (q *queue) display() {
+	if q.front == nil {
+		fmt.Println("Queue is empty")
+		return
+	}
+
+	ptr := q.front
+
+	for ptr != q.rear {
+		fmt.Println(ptr.data)
+		ptr = ptr.next
+	}
+	fmt.Println(ptr.data)
+}
+
+func (q *queue) enQueue(value int) {
+	newNode := &node{
+		value,
+		nil,
+	}
+
+	if q.front == nil {
+		q.front, q.rear = newNode, newNode
+		return
+	}
+
+	q.rear.next = newNode
+	q.rear = newNode
+
+}
+
+func (q *queue) deQueue() {
+	if q.front == nil {
+		fmt.Println("Queue is empty")
+		return
+	}
+
+	q.front = q.front.next
+}
